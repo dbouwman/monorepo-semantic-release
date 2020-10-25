4b6137a3c1a38e6aaaf24168b44e472bb0995bbe
Dave Bouwman <dave@davebouwman.com>
2020-10-25 10:14:48 -0600

    feat(package-a): add fn

--- packages/package-a/test/index.test.ts
+++ packages/package-a/test/index.test.ts
@@ -1,10 +1,16 @@
-import { greet } from "../src/index";
+import { greet, greetFromDave } from "../src/index";
 
 describe("Greeter", () => {
   it("should greet by name", () => {
     expect(greet("dave")).toBe(
       "Hello dave, this is package-a",
       "should greet by name"
+    );
+  });
+  it("should greet by name from dave", () => {
+    expect(greetFromDave("rob")).toBe(
+      "Hello rob, this is Dave",
+      "should greet by name from Dave"
     );
   });
 });
