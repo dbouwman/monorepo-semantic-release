7f4971a08568424e7fd313f4b585a8195805856e
Dave Bouwman <dave@davebouwman.com>
2020-11-07 16:24:36 -0700

    chore: fix test

--- packages/package-a/test/index.test.ts
+++ packages/package-a/test/index.test.ts
@@ -1,22 +1,22 @@
-import { greet, greetFromDave, greetFromTom } from "../src/index";
+import { greet, greetFromDave, greetFromTom } from '../src/index';
 
-describe("Greeter", () => {
-  it("should greet by name", () => {
-    expect(greet("dave")).toBe(
-      "Hello dave, this is package-a",
-      "should greet by name"
+describe('Greeter', () => {
+  it('should greet by name', () => {
+    expect(greet('dave')).toBe(
+      'Hello dave, this is package-a',
+      'should greet by name'
     );
   });
-  it("should greet by name from dave", () => {
-    expect(greetFromDave("rob")).toBe(
-      "Hello rob, this is Dave",
-      "should greet by name from Dave"
+  it('should greet by name from dave', () => {
+    expect(greetFromDave('rob')).toBe(
+      'Hello rob, this is Dave',
+      'should greet by name from Dave'
     );
   });
-  it("should greet by name from Tom", () => {
-    expect(greetFromTom("rob")).toBe(
-      "Hello rob, this is Tom, and I am somewhat skeptical of our robot overlords",
-      "should greet by name from Tom"
+  it('should greet by name from Tom', () => {
+    expect(greetFromTom('rob')).toBe(
+      'Hello rob, this is Tom, and I am somewhat skeptical of our robot overlords but Dave is hopeful',
+      'should greet by name from Tom'
     );
   });
 });
